64bf9460f96274c02e1327d74094a590b758248e
m-mohitmanuja <60345601+m-mohitmanuja@users.noreply.github.com>
2020-04-02 17:04:14 +0530

    Delete urls.py

--- eon_backend/eon_backend/authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
-from .views import Login
-
-app_name = 'authentication'
-
-urlpatterns = [
-    path('login', Login.as_view())
-]
