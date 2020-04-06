32d7d658a72683a50318e06fc524d7de0973eb00
mohit.manuja <mohit.manuja@hashedin.com>
2020-04-06 23:37:11 +0530

    removed constants

--- utils/constants.py
+++ utils/constants.py
@@ -1,8 +1,6 @@
 PAYMENT_CONSTANTS = dict(
     status=[(0, "SUCCESSFUL"), (1, "FAILED"), (2, "IN_PROGRESS"), (3, "DECLINED")],
-    type=[(0, "CREDIT"), (1, "DEBIT")],
     values=dict(
-        type=dict(CREDIT=0, DEBIT=1),
         status=dict(SUCCESSFUL=0, FAILED=1, IN_PROGRESS=2, DECLINED=3),
     ),
 )
