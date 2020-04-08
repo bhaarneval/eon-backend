3c7a7167da91021c8d881388e9bf6da36e3d8545
sagar saxena <sagar.saxena@hashedin.com>
2020-04-08 21:01:33 +0530

    Test Cases for Invititation APIs

---
+++ core/tests/test_invitation.py
@@ -0,0 +1,132 @@
+import json
+from rest_framework.test import APITestCase
+
+
+class InvitationTestCase(APITestCase):
+    fixtures = ['default.json']
+    def setUp(cls):
+        data = dict(email="user18@gmail.com", password="user123")
+        cls.user = cls.client.post('/authentication/login', json.dumps(data), content_type='application/json')
+        cls.token = cls.user.data['data']['access']
+        cls.ENDPOINT = "/core/event/invite"
+
+    def test_invitation_api_with_wrong_method(self):
+        response = self.client.put(
+            self.ENDPOINT, HTTP_AUTHORIZATION="Bearer {}".format(self.token)
+        )
+        self.assertEquals(response.status_code, 405)
+
+    def test_invitation_api_with_wrong_token(self):
+        response = self.client.put(
+            self.ENDPOINT, HTTP_AUTHORIZATION="Bearer {}".format('token')
+        )
+        self.assertEquals(response.status_code, 401)
+
+    def test_invitation_get_api(self):
+        response = self.client.get(
+            self.ENDPOINT, HTTP_AUTHORIZATION="Bearer {}".format(self.token),
+        )
+        self.assertEquals(response.status_code, 200)
+
+    def test_invitation_get_api_with_particular_event(self):
+        response = self.client.get(
+            self.ENDPOINT, {"event_id": 10}, HTTP_AUTHORIZATION="Bearer {}".format(self.token),
+        )
+        self.assertEquals(response.status_code, 200)
+
+    def test_invitation_get_api_with_particular_user(self):
+        response = self.client.get(
+            self.ENDPOINT, {"user_id": 9}, HTTP_AUTHORIZATION="Bearer {}".format(self.token),
+        )
+        self.assertEquals(response.status_code, 200)
+
+    def test_invitation_get_api_with_particular_invalid_parameters(self):
+        response = self.client.get(
+            self.ENDPOINT, {"event_id": 1, "user_id": 1}, HTTP_AUTHORIZATION="Bearer {}".format(self.token),
+        )
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(len(response.data['data']['invitee_list']), 0)
+
+    def test_invitation_post_api_with_invalid_event_id(self):
+        data = {"event": 1000,
+                "discount_percentage": 10,
+                "invitee_list": ["email@gmail.com"]}
+        response = self.client.post(
+            self.ENDPOINT, json.dumps(data), HTTP_AUTHORIZATION="Bearer {}".format(self.token),
+            content_type='application/json'
+        )
+        self.assertEquals(response.status_code, 400)
+
+    def test_invitation_post_api_with_valid_details(self):
+        data = {"event": 9,
+                "discount_percentage": 10,
+                "invitee_list": ["email@gmail.com", "email1@gmail.com"]
+                }
+        response = self.client.post(
+            self.ENDPOINT, json.dumps(data), HTTP_AUTHORIZATION="Bearer {}".format(self.token),
+            content_type='application/json'
+        )
+        self.assertEquals(response.status_code, 200)
+
+    def test_invitation_post_api_for_same_event_id_and_users(self):
+        data = {"event": 9,
+                "discount_percentage": 10,
+                "invitee_list": ["email@gmail.com"]
+                }
+        response = self.client.post(
+            self.ENDPOINT, json.dumps(data), HTTP_AUTHORIZATION="Bearer {}".format(self.token),
+            content_type='application/json'
+        )
+        prev_id = response.data['data']['invitee_list'][0].get('invitation_id')
+        data = {"event": 9,
+                "discount_percentage": 11,
+                "invitee_list": ["email@gmail.com"]
+                }
+        response = self.client.post(
+            self.ENDPOINT, json.dumps(data), HTTP_AUTHORIZATION="Bearer {}".format(self.token),
+            content_type='application/json'
+        )
+        self.assertEquals(prev_id, response.data['data']['invitee_list'][0].get('invitation_id'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_invitation_post_api_for_same_event_id_not_users(self):
+        data = {"event": 9,
+                "discount_percentage": 10,
+                "invitee_list": ["email1@gmail.com"]
+                }
+        response = self.client.post(
+            self.ENDPOINT, json.dumps(data), HTTP_AUTHORIZATION="Bearer {}".format(self.token),
+            content_type='application/json'
+        )
+        prev_id = response.data['data']['invitee_list'][0].get('invitation_id')
+        data = {"event": 9,
+                "discount_percentage": 11,
+                "invitee_list": ["email@gmail.com"]
+                }
+        response = self.client.post(
+            self.ENDPOINT, json.dumps(data), HTTP_AUTHORIZATION="Bearer {}".format(self.token),
+            content_type='application/json'
+        )
+        self.assertNotEquals(prev_id, response.data['data']['invitee_list'][0].get('invitation_id'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_invitation_post_api_for_same_user_not_event(self):
+        data = {"event": 9,
+                "discount_percentage": 10,
+                "invitee_list": ["email1@gmail.com"]
+                }
+        response = self.client.post(
+            self.ENDPOINT, json.dumps(data), HTTP_AUTHORIZATION="Bearer {}".format(self.token),
+            content_type='application/json'
+        )
+        prev_id = response.data['data']['invitee_list'][0].get('invitation_id')
+        data = {"event": 9,
+                "discount_percentage": 11,
+                "invitee_list": ["email@gmail.com"]
+                }
+        response = self.client.post(
+            self.ENDPOINT, json.dumps(data), HTTP_AUTHORIZATION="Bearer {}".format(self.token),
+            content_type='application/json'
+        )
+        self.assertNotEquals(prev_id, response.data['data']['invitee_list'][0].get('invitation_id'))
+        self.assertEquals(response.status_code, 200)
