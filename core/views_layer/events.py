196aad5f87e28f2b375af9fff953456997a2c50f
mohit.manuja <mohit.manuja@hashedin.com>
2020-04-08 23:03:04 +0530

    settings changed

--- core/views_layer/events.py
+++ core/views_layer/events.py
@@ -11,7 +11,7 @@ from utils.common import api_error_response, api_success_response
 
 
 class EventViewSet(ModelViewSet):
-    authentication_classes = (JWTAuthentication,)
+    # authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = Event.objects.all().select_related('type').annotate(event_type=F('type__type'))
     serializer_class = ListUpdateEventSerializer
