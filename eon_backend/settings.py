98f736ad13c8eeb4465ce2d13e6bf4ed58e89ebc
mohit.manuja <mohit.manuja@hashedin.com>
2020-04-09 11:14:48 +0530

    removed authentication from event

--- eon_backend/settings.py
+++ eon_backend/settings.py
@@ -24,7 +24,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'sc6#6@en-9r(-(6=*uiw1xsds&(xxpjea=0bzr%nozw=25$vj)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
