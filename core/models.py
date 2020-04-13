72ec6227c7da5feda9e2adec00ec22aa54ba826b
mohit.manuja <mohit.manuja@hashedin.com>
2020-04-13 15:33:54 +0530

    django-admin

--- core/models.py
+++ core/models.py
@@ -16,7 +16,7 @@ class Event(ActiveModel):
     name = models.CharField(max_length=256)
     type = models.ForeignKey(EventType, on_delete=models.DO_NOTHING)
     description = models.CharField(max_length=512)
-    date = models.DateField(verbose_name="Date Range Filter")
+    date = models.DateField()
     time = models.TimeField()
     location = models.CharField(max_length=512)
     images = models.CharField(max_length=512, null=True, blank=True)
