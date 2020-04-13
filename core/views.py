eeda197be57ca6117a933d03054153ea597af989
Mayank Diwedi <mayank.diwedi@hashedin.com>
2020-04-13 16:11:33 +0530

    Moved notification module to a seperate file

--- core/views.py
+++ core/views.py
@@ -1,17 +1,14 @@
 import json
 
-import jwt
 from django.db.models import F
 from rest_framework.views import APIView
-from rest_framework.authentication import get_authorization_header
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import authentication_classes, permission_classes, api_view
-from core.models import Event, Subscription, EventType, Notification
-from core.serializers import EventTypeSerializer, NotificationSerializer
-from eon_backend.settings import SECRET_KEY
+from core.models import Event, Subscription, EventType
+from core.serializers import EventTypeSerializer
 
-from utils.common import api_success_response, api_error_response
+from utils.common import api_success_response
 from utils.helper import send_email_sms_and_notification
 
 
@@ -74,49 +71,3 @@ def send_mail_to_a_friend(request):
                                     message=message,
                                     email_ids=email)
     return api_success_response(message="Mail send successfully", status=200)
-
-
-class NotificationView(APIView):
-    """API for Notification"""
-
-    serializer_class = NotificationSerializer
-    authentication_classes = (JWTAuthentication,)
-    permission_classes = (IsAuthenticated, )
-
-    def patch(self, request):
-
-        list_of_ids = request.data.get('notification_ids')
-
-        for notification_id in list_of_ids:
-            try:
-                notification = Notification.objects.get(id=notification_id)
-                notification.has_read = True
-                notification.save()
-            except Notification.DoesNotExist:
-                api_error_response(message="Notification Id ={id} does not exist".format(id=notification_id), status=400)
-
-        return api_success_response(message="Unread notification updated successfully", status=200)
-
-    def get(self, request):
-
-        token = get_authorization_header(request).split()[1]
-        payload = jwt.decode(token, SECRET_KEY)
-        user_id = payload['user_id']
-
-        if user_id is not None:
-            try:
-                notifications = Notification.objects.filter(user=user_id, has_read=False)
-
-            except Notification.DoesNotExist:
-                notifications = []
-        else:
-            return api_error_response(message="user ID can not be null", status=400)
-        json_list = []
-        for notification in notifications:
-            notification_obj = {
-                "message": notification.message,
-                "notification_id": notification.id
-            }
-            json_list.append(notification_obj)
-
-        return api_success_response(data=json_list)
---
+++ core/views_layer/notification.py
@@ -0,0 +1,53 @@
+import jwt
+
+from rest_framework.views import APIView
+from rest_framework.authentication import get_authorization_header
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Notification
+from core.serializers import NotificationSerializer
+from eon_backend.settings import SECRET_KEY
+
+from utils.common import api_success_response, api_error_response
+
+
+class NotificationView(APIView):
+    """API for Notification"""
+
+    serializer_class = NotificationSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated, )
+    queryset = Notification.objects.all()
+
+    def patch(self, request):
+
+        list_of_ids = request.data.get('notification_ids')
+
+        try:
+            self.queryset.filter(id__in=list_of_ids).update(has_read=True)
+        except:
+            api_error_response(message="Something went wrong", status=500)
+
+        return api_success_response(message="Notification updated successfully", status=200)
+
+    def get(self, request):
+
+        token = get_authorization_header(request).split()[1]
+        payload = jwt.decode(token, SECRET_KEY)
+        user_id = payload['user_id']
+
+        try:
+            notifications = self.queryset.filter(user=user_id, has_read=False)
+
+        except Notification.DoesNotExist:
+            notifications = []
+        json_list = []
+        for notification in notifications:
+            notification_obj = {
+                "message": notification.message,
+                "notification_id": notification.id,
+            }
+            json_list.append(notification_obj)
+
+        return api_success_response(data=json_list)
