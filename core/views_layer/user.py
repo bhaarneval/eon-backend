2a4ffaace71cfea35fd774983430eb8fa483466b
sagar saxena <sagar.saxena@hashedin.com>
2020-04-13 17:19:36 +0530

    Is authenticated enabled for userprofile API

--- core/views_layer/user.py
+++ core/views_layer/user.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from authentication.models import User
 from core.models import UserProfile, UserInterest
 from core.serializers import UserProfileSerializer
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import get_authorization_header
 from utils.common import api_error_response, api_success_response
 from eon_backend.settings import SECRET_KEY
@@ -12,6 +13,7 @@ import jwt
 
 class UserViewSet(ModelViewSet):
     authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     lookup_field = "user_id"
@@ -38,7 +40,7 @@ class UserViewSet(ModelViewSet):
                 UserInterest.objects.filter(event_type_id__in=interest_to_be_deleted).update(is_active=False)
             except:
                 prev_interest = []
-                return api_error_response(message="Some error occur in deleting interests", status=500)
+                return api_error_response(message="Something went wrong", status=500)
 
             for current in interests:
                 try:
@@ -50,7 +52,7 @@ class UserViewSet(ModelViewSet):
             response = serializer.data
             response['interest'] = interest_list
         except Exception as err:
-            return api_error_response(message="Some internal error coming while updating the event", status=500)
+            return api_error_response(message="Something went wrong", status=500)
         return api_success_response(data=response, status=200)
 
     def list(self, request, *args, **kwargs):
@@ -62,13 +64,13 @@ class UserViewSet(ModelViewSet):
             user_logged_in = user_id
             user_role = UserProfile.objects.get(user_id=user_logged_in).role.role
         except Exception as err:
-            return api_error_response(message="Not able to fetch the role of the logged in user", status=500)
+            return api_error_response(message="Something went wrong", status=500)
         data = []
         for profile in self.queryset:
             curr_profile = {'id': profile.user.id, 'name': profile.name, 'contact_number': profile.contact_number,
                             'address': profile.address, 'role': profile.role.id, 'organization': profile.organization}
             try:
-                list_of_interest = list(UserInterest.objects.filter(user=profile.user.id,is_active=True).
+                list_of_interest = list(UserInterest.objects.filter(user=profile.user.id, is_active=True).
                                         values_list('event_type', flat=True))
             except UserInterest.DoesNotExist:
                 list_of_interest = []
@@ -91,11 +93,12 @@ class UserViewSet(ModelViewSet):
         curr_profile= {'id': profile.user.id, 'name': profile.name, 'contact_number': profile.contact_number,
                        'address': profile.address, 'role': profile.role.id, 'organization': profile.organization}
         try:
-            list_of_interest = list(UserInterest.objects.filter(user=user_id,is_active=True).values_list('event_type', flat=True))
+            list_of_interest = list(UserInterest.objects.filter(user=user_id, is_active=True).
+                                    values_list('event_type', flat=True))
         except UserInterest.DoesNotExist:
             list_of_interest = []
 
         curr_profile['interest'] = list_of_interest
 
-        return api_success_response(data= curr_profile,message="user details", status=200)
+        return api_success_response(data=curr_profile, message="user details", status=200)
 
