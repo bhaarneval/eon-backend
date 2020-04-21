72b5888066eb543f2ea09b961293f867bbc43c55
mohit.manuja <mohit.manuja@hashedin.com>
2020-04-21 22:43:20 +0530

    event_report_analysis

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,10 @@
 In this file we added all the url reference
 """
 from django.conf.urls import url
-from django.urls import include
+from django.urls import include, re_path
 
 from core.presigned_url import PresignedUrl
+from core.reports import event_analysis_report, filtered_event_summary, event_summary
 from core.routes import router
 from core.views import get_event_types, SubscriberNotify, send_mail_to_a_friend
 from core.views_layer.invitation import InvitationViewSet
@@ -17,5 +18,7 @@ urlpatterns = [
     url('notify-subscriber', SubscriberNotify.as_view(), name="subscriber_notify"),
     url("event-type", get_event_types, name="event_type"),
     url("share-with-friend", send_mail_to_a_friend, name="share_with_friend"),
-    url('notification', NotificationView.as_view(), name='notification')
+    url('notification', NotificationView.as_view(), name='notification'),
+    url('reports', event_summary, name='report'),
+    re_path('filtered_event_summary', filtered_event_summary, name="filtered_event_summary"),
 ]
