b885eb01581d3a999838506b010ce80b5e1b5860
mohit.manuja <mohit.manuja@hashedin.com>
2020-04-22 17:43:05 +0530

    payment removed

--- payment/admin.py
+++ payment/admin.py
@@ -7,21 +7,21 @@ from django.contrib import admin
 from payment.models import Payment
 
 
-@admin.register(Payment)
-class PaymentAdmin(admin.ModelAdmin):
-    """
-    Method to show filds for admin
-    """
-    list_display = ("id", 'amount', 'discount_amount', 'total_amount', 'status', 'ref_number')
-    list_filter = ("status",)
-    search_fields = ('id', 'status', 'ref_number')
-    readonly_fields = ('amount', 'discount_amount', 'total_amount', 'status', 'ref_number')
-
-    def has_delete_permission(self, request, obj=None):
-        return False
-
-    def has_add_permission(self, request):
-        return False
-
-    def has_change_permission(self, request, obj=None):
-        return False
+# @admin.register(Payment)
+# class PaymentAdmin(admin.ModelAdmin):
+#     """
+#     Method to show filds for admin
+#     """
+#     list_display = ("id", 'amount', 'discount_amount', 'total_amount', 'status', 'ref_number')
+#     list_filter = ("status",)
+#     search_fields = ('id', 'status', 'ref_number')
+#     readonly_fields = ('amount', 'discount_amount', 'total_amount', 'status', 'ref_number')
+#
+#     def has_delete_permission(self, request, obj=None):
+#         return False
+#
+#     def has_add_permission(self, request):
+#         return False
+#
+#     def has_change_permission(self, request, obj=None):
+#         return False
