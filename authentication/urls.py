4e44fe147034efc5b1b4afec24ddf134f357f211
mohit.manuja <mohit.manuja@hashedin.com>
2020-04-30 23:29:12 +0530

    new token changes

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,7 +3,7 @@ In this file we added all the url reference
 """
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from .views import Login, Register, change_user_password, reset_password, send_forget_password_mail, verify_token
+from .views import Login, Register, change_user_password, reset_password, send_forget_password_mail
 
 urlpatterns = [
     path('login', Login.as_view(), name='login'),
@@ -11,6 +11,5 @@ urlpatterns = [
     path('token-refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('change-password', change_user_password, name='change-password'),
     path('reset-password', reset_password, name='reset-password'),
-    path('generate-code', send_forget_password_mail, name='send_verification_code'),
-    path('verify-token', verify_token, name='verify_token')
+    path('generate-code', send_forget_password_mail, name='send_verification_code')
 ]
