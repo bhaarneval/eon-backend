1c645fcc7775ed0843c31f4992048b1a0dd447d1
ranjankumarhashedin <62424871+ranjankumarhashedin@users.noreply.github.com>
2020-05-06 23:53:03 +0530

    Update base.py

--- eon_backend/settings/common/base.py
+++ eon_backend/settings/common/base.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = ['dev-env-bits-pilani-backend.us-east-1.elasticbeanstalk.com',
                  'BitsPilaniEonBackend-env.eba-iewfgdnb.us-east-1.elasticbeanstalk.com',
-                 'http://bitspilanieonbackenddeployebs-env.eba-v3hw7gqp.ap-south-1.elasticbeanstalk.com',
+                 'bitspilanieonbackenddeployebs-prod.eba-v3hw7gqp.ap-south-1.elasticbeanstalk.com',
                  'localhost', '127.0.0.1', '[::1]', 'https://backend-eb.bits-pilani-eon.net']
 
 # Application definition
